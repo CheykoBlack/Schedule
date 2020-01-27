816ef8fdeffb0a97776e494096558d6dad126f13
Chelsey <cmartin@apterainc.com>
2020-01-27 12:00:14 -0500

    wip: initial commit

--- Database/Startup.cs
+++ Database/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Schedule
+namespace Database
 {
     public class Startup
     {
@@ -16,7 +16,6 @@ namespace Schedule
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,9 +30,10 @@ namespace Schedule
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
---
+++ Schedule.EntityFramework/ScheduleContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Schedule.EntityFramework
+{
+    class ScheduleContext : DbContext
+    {
+        public ScheduleContext() : base("Name=DefaultConnection") { }
+    }
+}
